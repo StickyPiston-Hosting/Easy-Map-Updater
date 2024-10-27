868662672e9c9e899cc42a1d621bc9884a5643eb
Dominexis <dominexis9@gmail.com>
2024-10-27 19:26:15 +0100

    Added fix for fire_resistant component

--- lib/data_pack_files/item_component.py
+++ lib/data_pack_files/item_component.py
@@ -324,6 +324,10 @@ def conform_component(component: ItemComponent, version: int):
                 levels[updated_enchantment] = nbt_tags.TypeInt(min(max(levels[updated_enchantment].value, 0), 255))
         component.value = enchantments
 
+    if component.key == "minecraft:fire_resistant":
+        component.key = "minecraft:damage_resistant"
+        component.value = {"types": "#minecraft:is_fire"}
+
     if component.key == "minecraft:item_name":
         component.value = json_text_component.update(component.value, version, [], True)
 
